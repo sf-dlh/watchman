7613f03f47cfa4c0ca437b06f2b9f9818f1bfc10
sf-dlh <sfe.abdl@gmail.com>
2026-02-20 05:26:38 +0100

    modified user_data for ec2 startup

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,34 +1,47 @@
 data "aws_ami" "ubuntu" {
-    most_recent = true
-    owners = ["099720109477"]
+  most_recent = true
+  owners      = ["099720109477"]
 
-    filter {
-        name = "name"
-        values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-    }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
 }
 
 resource "aws_instance" "watchman" {
-    ami = data.aws_ami.ubuntu.id
-    instance_type = "t3.micro"
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
 
-    subnet_id = aws_subnet.watchman_vpc_subnet.id
+  subnet_id = aws_subnet.watchman_vpc_subnet.id
 
-    vpc_security_group_ids = [aws_security_group.ssh_ok.id]
+  vpc_security_group_ids = [aws_security_group.ssh_ok.id]
 
-    iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
-  
-    key_name = "watchman"
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
-    user_data = <<-EOF
+  key_name = "watchman"
+
+  user_data = <<-EOF
         #!/bin/bash
         apt-get update -y
         apt-get install docker.io -y
+        apt-get install unzip curl -y
         systemctl enable --now docker
         usermod -aG docker ubuntu
+
+        curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+        unzip awscliv2.zip
+        ./aws/install
+
+        aws ecr get-login-password | docker login --username AWS --password-stdin ${data.aws_caller_identity.me.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com
+        docker pull ${aws_ecr_repository.watchman_repo.repository_url}:0.3
+
+        docker run -d --name watchman --restart always ${aws_ecr_repository.watchman_repo.repository_url}:0.3
         EOF
 
-    tags = {
-      Name = "Watchman_Host"
-    }
+  tags = {
+    Name = "Watchman_Host"
+  }
 }
+
+// to get account ID
+data "aws_caller_identity" "me" {}
