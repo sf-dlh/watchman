a67a8a09a53c0c14255040bd5d63fa8853726f6c
sf-dlh <sfe.abdl@gmail.com>
2026-02-20 23:04:08 +0100

    changed to remote s3 backend instead of local for a stateless infra model

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -5,5 +5,17 @@ terraform {
 			version = "~> 5.92"			
 		}	
 	}
+
+	backend "s3" {
+		bucket = "sf-watchman-state"
+		key = "watchman/terraform.tfstate"
+		region = "us-east-1"
+		// for native state locking, thought about using dynamodb but HC's documentation says its being deprecated
+		use_lockfile = true
+	}
+
+
+
+
 	required_version = ">= 1.2"
 }
